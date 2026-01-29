d2a0f94bec46126cd8abb3cfdc68c8fd02f75cef
Doozy <d00zyl1v3@gmail.com>
2026-01-29 11:10:46 +0100

    added a loading bar added a pop-up to warn that there are already score files in the folder made the browser run headlessly

--- src/MusescoreDL/MusescoreDL.py
+++ src/MusescoreDL/MusescoreDL.py
@@ -2,38 +2,53 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common import exceptions as selenium_exceptions
 import requests
 import os
 import typer
 from typing_extensions import Annotated
+from tqdm import tqdm
+import re
 
-MAXWAITTIME = 30
-
-def DownloadScore(url: Annotated[str, typer.Argument(help="The URL to the score you want to download")], folder_path: Annotated[str, typer.Option(help="The complete path to the folder you want the scores to be put in")] = os.getcwd()):
-    driver = webdriver.Firefox()
-    driver.minimize_window()
-    driver.get(url)
-
-    WebDriverWait(driver, MAXWAITTIME).until(EC.presence_of_element_located((By.ID, "jmuse-scroller-component")))
-    pages = driver.find_elements(By.CLASS_NAME, "A8huy")
-    for i in range(len(pages)):
-        page = pages[len(pages)-i-1]
-        driver.execute_script("arguments[0].scrollIntoView(true);", page)
-        WebDriverWait(driver, MAXWAITTIME).until(EC.presence_of_element_located((By.XPATH, f"/html/body/div[2]/div[1]/section/main/div/div[3]/div/div/div[{len(pages)-i}]/img")))
-        img = driver.find_element(By.XPATH, f"/html/body/div[2]/div[1]/section/main/div/div[3]/div/div/div[{len(pages)-i}]/img")
-        WebDriverWait(driver, MAXWAITTIME).until(lambda _: img.get_attribute("src") != None)
-        src = img.get_attribute("src")
-        assert src != None
-        with open(os.path.join(folder_path,f"score_{len(pages)-i-1}.svg"), "x+") as f:
-            if len(pages)-i > 1:
-                response = requests.get(src)
-                if response.status_code != 200:
-                    raise ConnectionError(f"Something went wrong while downloading sheet {len(pages)-i}")
-                f.write(response.text)
-            else:
-                driver.get(src)
-                f.write(driver.page_source)
-    driver.close()
+def DownloadScore(url: Annotated[str, typer.Argument(help="The URL to the score you want to download")], folder_path: Annotated[str, typer.Option(help="The complete path to the folder you want the scores to be put in")] = os.getcwd(), max_wait_timeout: Annotated[int, typer.Option(help="The amount of time to wait for a page to load before concluding something has failed (Only change when wifi is slow)")] = 30):
+    score_pattern = re.compile(r"score_\d\.svg")
+    if any([score_pattern.match(file) for file in os.listdir(folder_path)]):
+        if input("Found a score in the folder, the program will not be able to run unless this is either moved or deleted.\nDo you want to delete all the scores in the folder? (y/n)    ").lower() == "y":
+            
+            for file in os.listdir(folder_path):
+                if score_pattern.match(file):
+                    os.remove(file)
     
+    firefox_options = webdriver.FirefoxOptions()
+    firefox_options.add_argument("--headless")
+    driver = webdriver.Firefox(options=firefox_options)
+    driver.get(url)
+    try:
+        WebDriverWait(driver, max_wait_timeout).until(EC.presence_of_element_located((By.ID, "jmuse-scroller-component")))
+        pages = driver.find_elements(By.CLASS_NAME, "A8huy")
+        
+        for i in tqdm(range(len(pages)), unit="page"):
+            page = pages[len(pages)-i-1]
+            driver.execute_script("arguments[0].scrollIntoView(true);", page)
+            WebDriverWait(driver, max_wait_timeout).until(EC.presence_of_element_located((By.XPATH, f"/html/body/div[2]/div[1]/section/main/div/div[3]/div/div/div[{len(pages)-i}]/img")))
+            img = driver.find_element(By.XPATH, f"/html/body/div[2]/div[1]/section/main/div/div[3]/div/div/div[{len(pages)-i}]/img")
+            WebDriverWait(driver, max_wait_timeout).until(lambda _: img.get_attribute("src") != None)
+            src = img.get_attribute("src")
+            assert src != None
+            with open(os.path.join(folder_path,f"score_{len(pages)-i-1}.svg"), "x+") as f:
+                if len(pages)-i > 1:
+                    response = requests.get(src)
+                    if response.status_code != 200:
+                        raise ConnectionError(f"Something went wrong while downloading sheet {len(pages)-i}")
+                    f.write(response.text)
+                else:
+                    driver.get(src)
+                    f.write(driver.page_source)
+        driver.close()
+        
+    except selenium_exceptions.TimeoutException:
+        driver.close()
+        raise TimeoutError("The page took too long to load. If nothing went wrong, try again with a higher max_wait_timeout")
+
 if __name__ == "__main__":
     DownloadScore(input("Musescore URL:    "))
